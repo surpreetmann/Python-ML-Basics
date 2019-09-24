2789664ca45aec66a68002294da9928fbb3f8128
surpreetmann <surpreetmann35@gmail.com>
2019-09-24 23:48:20 +0530

    file

--- plotting2d.py
+++ plotting2d.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 def f(x1,x2):
   return((x1-1)**2+(x2-2)**2)
 
-x1=np.linspace(-10,10,500)
-x2=np.linspace(-10,10,500)
+x1=np.linspace(-10,10,300)
+x2=np.linspace(-10,10,300)
 x1,x2=np.meshgrid(x1,x2)  
 z=f(x1,x2)
 plt.contour(x1,x2,z,20)
